d40c8d2d79be13facd5c592d125e1a4e53466b87
김영현 <200958@jnu.ac.kr>
2023-03-12 09:51:06 +0900

    [INIT] Start v2.0
    Version 2.0 is .exe program which has GUI.
    User will shouldn't input personal information onto program.

--- user.py
+++ user.py
@@ -3,11 +3,11 @@ login_info = {
     'pw': "your_pw"     # Input your CNU portal password.
 }
 
-class_name = "class_name" 
+class_name = "리눅스" 
 # Input class name what you want to play automatically.
 # It's fine that you input part of class name.
 
-start_lecture_name = "start_lecture_name"
+start_lecture_name = ""
 # Input class name what you want to play first.
 # It's fine that you input part of lecture name.
 # If you set ' '(blank) or default value, the lectures will be played sequentially from the beginning.
